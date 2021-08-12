121f1b5781fe412ecb1744a95bd36fa78f047fa9
FelipeSSac <felipe_adoubs@outlook.com>
2021-08-12 16:56:59 -0300

    button hover effect

--- src/assets/styles/global.ts
+++ src/assets/styles/global.ts
@@ -19,6 +19,12 @@ export default createGlobalStyle`
 
   button {
     cursor: pointer;
+    transition: filter 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
+
+    &:hover{
+      filter: brightness(0.9);
+      box-shadow: inset 0 0 10px rgba(0,0,0,0.3);
+    }
   }
 
   @media (min-width:691px) and (max-width:955px){
