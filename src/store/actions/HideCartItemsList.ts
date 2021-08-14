84db322124f90f9fb78ebf78abe4ccbe2573586f
FelipeSSac <felipe_adoubs@outlook.com>
2021-08-14 13:54:24 -0300

    on click out fix

--- src/store/actions/HideCartItemsList.ts
+++ src/store/actions/HideCartItemsList.ts
@@ -1,6 +1,8 @@
 import { IState } from '../state';
 
 export default function HideCartItemsList(state:IState) {
+  if (state.showCartItemsList === false) return state;
+
   return ({
     ...state,
     showCartItemsList: false,
